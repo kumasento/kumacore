fd1d72b11f13464400caf99d083b022dbe69e264
Vincent Zhao <vincentzhaorz@gmail.com>
2016-01-16 16:51:12 +0800

    revised port

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const server = require('./config/express.config'),
       twit   = require('./config/twitter.config');
       io     = require('socket.io')(server);
 
-server.listen(3000, function () {
-  console.log('Express server listen at 3000 ...');
+var PORT = process.env.PORT || 3000;
+
+server.listen(PORT, function () {
+  console.log('Express server listen at %d ...', PORT);
 });
